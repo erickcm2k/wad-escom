3f1814cc924faab85910dc9bfe65512165c4ef98
erickcm2k <erickce40@gmail.com>
2022-03-22 19:16:12 -0600

    l1 terminada

---
+++ l1/Registro.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Registro extends HttpServlet {
+
+    /**
+     * Manipular las posibles peticiones enviadas por el cliente
+     * utilizando el atributo method=get o method=post
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("ap1");
+
+
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet Registro</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Servlet Registro del alumno: " + nombre + apellido +" exitoso. </h1>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+
+    }
+
+    /**
+     * Manipular la petición enviada por el cliente
+     * utilizando el atributo method=get
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Manipular la petición enviada por el cliente
+     * utilizando el atributo method=post
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Devuelve una descripción breve del servlet.
+     */
+    @Override
+    public String getServletInfo() {
+        return "Servlet Registro";
+    }// </editor-fold>
+
+}
